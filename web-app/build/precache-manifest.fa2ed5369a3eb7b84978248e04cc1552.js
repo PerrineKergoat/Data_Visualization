8c8f2c9140ac3d54c264088f79d4c1d5ad1e9634
Gavinkaa <ludovic.burnier@epfl.ch>
2023-05-05 16:48:28 +0200

    add missing static

---
+++ web-app/build/precache-manifest.fa2ed5369a3eb7b84978248e04cc1552.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "0ac3fe617eda6f5c4c38",
+    "url": "/project-2023-lcp-data-wizards/static/css/main.57340a6f.chunk.css"
+  },
+  {
+    "revision": "0ac3fe617eda6f5c4c38",
+    "url": "/project-2023-lcp-data-wizards/static/js/main.0ac3fe61.chunk.js"
+  },
+  {
+    "revision": "03d3cddbd4bb69226339",
+    "url": "/project-2023-lcp-data-wizards/static/js/1.03d3cddb.chunk.js"
+  },
+  {
+    "revision": "45e0d8e1113f9e4d4f6b",
+    "url": "/project-2023-lcp-data-wizards/static/js/runtime~main.45e0d8e1.js"
+  },
+  {
+    "revision": "9d56c7b661ae391cfede802f042c35ed",
+    "url": "/project-2023-lcp-data-wizards/static/media/map.9d56c7b6.jpg"
+  },
+  {
+    "revision": "79b993f96c13b2aa130fee366b086556",
+    "url": "/project-2023-lcp-data-wizards/static/media/vulnerabilities.79b993f9.jpg"
+  },
+  {
+    "revision": "69e840e3731038c444575e435721eaf0",
+    "url": "/project-2023-lcp-data-wizards/static/media/distrib.69e840e3.jpg"
+  },
+  {
+    "revision": "35c639136979f6db22c37e63aed8d7d9",
+    "url": "/project-2023-lcp-data-wizards/static/media/terrestrial.35c63913.jpg"
+  },
+  {
+    "revision": "d54b9fe9f23982798e052fe0cf5d062f",
+    "url": "/project-2023-lcp-data-wizards/static/media/marine.d54b9fe9.jpg"
+  },
+  {
+    "revision": "919446b3e8310e40e3e50f9762f67c3c",
+    "url": "/project-2023-lcp-data-wizards/static/media/tree.919446b3.jpg"
+  },
+  {
+    "revision": "ea2c11832aeac2055e44371a71733863",
+    "url": "/project-2023-lcp-data-wizards/static/media/treeCountry.ea2c1183.jpg"
+  },
+  {
+    "revision": "0c40474d1fd45195fa96e2e140f2a610",
+    "url": "/project-2023-lcp-data-wizards/static/media/treeWorld.0c40474d.jpg"
+  },
+  {
+    "revision": "7b390023e0e64e358ce1655d096f2f59",
+    "url": "/project-2023-lcp-data-wizards/static/media/logo.7b390023.svg"
+  },
+  {
+    "revision": "67d352e09c69a1eb21a5ba7d804c9d33",
+    "url": "/project-2023-lcp-data-wizards/index.html"
+  }
+];

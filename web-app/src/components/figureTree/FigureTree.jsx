513ea4974e4e83788327a3f7d2af9f8499ebadca
Perrine Kergoat <115696643+PerrineKergoat@users.noreply.github.com>
2023-06-04 14:33:18 +0200

    Update FigureTree.jsx

--- web-app/src/components/figureTree/FigureTree.jsx
+++ web-app/src/components/figureTree/FigureTree.jsx
@@ -1,18 +1,3 @@
-import React from 'react';
-import tree from '../../assets/tree.jpg';
-import './figureTree.css';
-
-const FigureTree = () => {
-    return (
-        <div className="figureTree" id="figureTree">
-            <div className="figureTree__card">
-                <img src={tree} alt="tree" id="imgTree"/>
-            </div>
-        </div>
-    );
-};
-
-export default FigureTree;
 
 
 const treeData =[{
